e4601c00157b90c7bf06265b03438546e44a00c1
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-18 15:07:08 +0530

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Mycomponents/Header';
-import Todos from './Mycomponents/Todos';
-import Footer from './Mycomponents/Footer';
+// import Todos from './Mycomponents/Todos';
+// import Footer from './Mycomponents/Footer';
 
 function App() {
   return (
     <>
       <Header/>
-      <Todos/>
-      <Footer/>
+      {/* <Todos/>
+      <Footer/> */}
     </>
 
   );
