6a4a516d3c0a3489be31e8d71b1425fee81cf07d
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-18 20:47:40 +0530

    Update Todos.js

--- my-app/src/Mycomponents/Todos.js
+++ my-app/src/Mycomponents/Todos.js
@@ -4,8 +4,9 @@ import Todo  from './Todo';
 export default function Todos(props) {
   return (
     <div className='container'>
-      <h3 className='text-center'>Todos List</h3>
+      <h3 className='text-center my-4'>Todos List</h3>
       <Todo todo={props.Todos[0]}/>
     </div>
   );
 }
+// 1.3.45
